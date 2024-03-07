15446bad94dd0f117c35f741fcc94cb9764ed5d8
yassinerahhou <yassine.rahhou.1@gmail.com>
2024-03-07 20:20:59 +0000

    fix errors

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,7 +3,6 @@
 0-pascal_triangle
 """
 
-
 def pascal_triangle(n):
     """
     0-pascal_triangle
@@ -12,8 +11,7 @@ def pascal_triangle(n):
         return []
     
     liste_1 = []
-    liste_2 = [] 
-
+    liste_2 = []
 
     for i in range(n):
         listeP = []
@@ -21,8 +19,7 @@ def pascal_triangle(n):
             if j == 0 or j == i:
                 listeP.append(1)
             else:
-                liste_2 = liste_1[i-1]  
+                liste_2 = liste_1[i-1]
                 listeP.append(liste_2[j-1] + liste_2[j])
-            
         liste_1.append(listeP)
-    return(liste_1)
+    return liste_1
